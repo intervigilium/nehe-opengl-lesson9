380f7c8285a55567cdeb3b99c15fb997c94074b5
Ethan Chen <intervigil@gmail.com>
2010-08-31 18:15:31 -0700

    change some opengl texture flags when loading texture

--- jni/gltutorial/utils.c
+++ jni/gltutorial/utils.c
@@ -139,9 +139,9 @@ GLuint loadTextureFromPNG(const char* filename, int* width, int* height) {
   GLuint texture;
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, *width, *height, 0, GL_RGBA,
-      GL_UNSIGNED_BYTE, (GLvoid*) image_data);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, *width, *height, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*) image_data);
 
   //clean up memory and close stuff
   png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
