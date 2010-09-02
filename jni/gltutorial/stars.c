d67757f66d9330547e4d14e819379c26a65f9b18
Ethan Chen <intervigil@gmail.com>
2010-09-01 20:58:20 -0700

    enable/disable client draw state

--- jni/gltutorial/stars.c
+++ jni/gltutorial/stars.c
@@ -83,18 +83,26 @@ int drawGLScene() {
 			glColor4ub(stars[NUM_STARS - i].r, stars[NUM_STARS - i].g, stars[NUM_STARS - i].b, 255);
 			// draw twinkle here
 			//glDrawTexfOES(0.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+			glEnableClientState(GL_VERTEX_ARRAY);
+			glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 			glVertexPointer(3, GL_FLOAT, 0, vertices);
 			glTexCoordPointer(2, GL_FLOAT, 0, texCoords);
 			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+			glDisableClientState(GL_VERTEX_ARRAY);
+			glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 		}
 		// rotate the uncolored star here
 		glRotatef(spin, 0.0f, 0.0f, 1.0f);
 		glColor4ub(stars[i].r, stars[i].g, stars[i].b, 255);
 		// draw star here
 		//glDrawTexfOES(0.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 		glVertexPointer(3, GL_FLOAT, 0, vertices);
 		glTexCoordPointer(2, GL_FLOAT, 0, texCoords);
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 
 		spin += 0.01f;
 		stars[i].angle += ((float) i)/NUM_STARS;
