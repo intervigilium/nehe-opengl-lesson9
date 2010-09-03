2e243a97e0c9ea661a9b1190b425a5162d35db4b
Ethan Chen <intervigil@gmail.com>
2010-09-02 18:23:15 -0700

    use RGBA for PNGs

--- jni/gltutorial/utils.c
+++ jni/gltutorial/utils.c
@@ -141,7 +141,7 @@ GLuint loadTextureFromPNG(const char* filename, int* width, int* height) {
   glBindTexture(GL_TEXTURE_2D, texture);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, *width, *height, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*) image_data);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, *width, *height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (GLvoid*) image_data);
 
   //clean up memory and close stuff
   png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
