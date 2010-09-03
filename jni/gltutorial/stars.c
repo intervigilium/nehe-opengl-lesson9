d7e09f25aaab45949af02df60ac40579671322e9
Ethan Chen <intervigil@gmail.com>
2010-09-02 18:23:49 -0700

    remove commented out crap

--- jni/gltutorial/stars.c
+++ jni/gltutorial/stars.c
@@ -81,8 +81,6 @@ int drawGLScene() {
 
 		if (TWINKLE_ON) {
 			glColor4ub(stars[NUM_STARS - i].r, stars[NUM_STARS - i].g, stars[NUM_STARS - i].b, 255);
-			// draw twinkle here
-			//glDrawTexfOES(0.0f, 0.0f, 0.0f, 1.0f, 1.0f);
 			glEnableClientState(GL_VERTEX_ARRAY);
 			glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 			glVertexPointer(3, GL_FLOAT, 0, vertices);
@@ -94,8 +92,7 @@ int drawGLScene() {
 		// rotate the uncolored star here
 		glRotatef(spin, 0.0f, 0.0f, 1.0f);
 		glColor4ub(stars[i].r, stars[i].g, stars[i].b, 255);
-		// draw star here
-		//glDrawTexfOES(0.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+
 		glEnableClientState(GL_VERTEX_ARRAY);
 		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 		glVertexPointer(3, GL_FLOAT, 0, vertices);
@@ -103,6 +100,7 @@ int drawGLScene() {
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 		glDisableClientState(GL_VERTEX_ARRAY);
 		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+
 
 		spin += 0.01f;
 		stars[i].angle += ((float) i)/NUM_STARS;
